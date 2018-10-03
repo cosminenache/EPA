4cc76e2ab9762053dd38247d95cc1954d651c968
epauser <epauser@localhost.localdomain>
2018-10-03 21:14:58 +0100

    CPU counter

---
+++ Week2/count.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#this file contains info on the number of cores on the VM
+# /proc/cpuinfo
+
+#grep is used to search for a string:
+
+no_core_cpu=`grep -i processor /proc/cpuinfo | wc -l`
+
+if [ $no_core_cpu -lt $1 ] 
+	then exit
+fi
+
+echo $no_core_cpu
