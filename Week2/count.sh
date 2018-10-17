ba61387c73eb1d7036199b5ad192b508209514ba
epauser <epauser@localhost.localdomain>
2018-10-17 20:28:19 +0100

    Validate user input

--- Week2/count.sh
+++ Week2/count.sh
@@ -9,8 +9,16 @@
 
 no_core_cpu=$(grep -i processor /proc/cpuinfo | wc -l)
 
-if [ $no_core_cpu -lt $1 ] 
-	then exit
+if [ -n "$1" ]; then
+	if [[ "$1" =~ ^[0-9]+$ ]]; then
+		if [ $no_core_cpu -lt $1 ]; then 
+	        	echo "Insufficent CPUs, install abandoned"	
+  		else
+			echo "Number of CPUs is: $no_core_cpu"
+        	fi
+	else
+		echo "Param $1 is not an integer"
+	fi
+else
+	echo "Param \$1 not provided"
 fi
-
-echo $no_core_cpu
