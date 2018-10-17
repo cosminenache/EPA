8e82dda33327fedaab0d0e04ac0af9dbe207dbcc
epauser <epauser@localhost.localdomain>
2018-10-17 21:03:59 +0100

    Add comments to the script

--- Week2/count.sh
+++ Week2/count.sh
@@ -7,10 +7,14 @@
 
 #no_core_cpu=`grep -i processor /proc/cpuinfo | wc -l`
 
+# get the number of CPUs on the machine
 no_core_cpu=$(grep -i processor /proc/cpuinfo | wc -l)
 
+# check if input has been provided at the command line
 if [ -n "$1" ]; then
+	#check if the input is an integer
 	if [[ "$1" =~ ^[0-9]+$ ]]; then
+		#check if the number of CPUs is less than the param input at the command line
 		if [ $no_core_cpu -lt $1 ]; then 
 	        	echo "Insufficent CPUs, install abandoned"	
   		else
