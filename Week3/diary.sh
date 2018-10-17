c151e83bd004983c8cccb5c4fb81fab2d4f34c20
epauser <epauser@localhost.localdomain>
2018-10-17 21:02:13 +0100

    Updated scripts with comments

--- Week3/diary.sh
+++ Week3/diary.sh
@@ -2,6 +2,8 @@
 
 # Read input from the command line
 read entry
+# Get the system date and format it YYYY-MM-DD:HH24:MI:SS
 current_date=$(date '+%Y-%m-%d:%H:%M:%S')
 
+# Append sys date and input from the terminat to the diary
 echo $current_date \>\> $entry >> Diary.txt
